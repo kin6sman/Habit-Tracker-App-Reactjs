82ba981e57cba055ee1271a857abb876c99c3922
kin6sman <domin12313xyz@gmail.com>
2023-05-31 10:35:35 +0530

    created main habit page

--- src/components/CreateHabit.jsx
+++ src/components/CreateHabit.jsx
@@ -1,8 +1,63 @@
 import React from 'react'
+import { useState } from 'react';
+import { Model, Button, Form } from 'react-bootstrap'
+import { useDispatch } from 'react-redux';
+import { addHabit } from '../features/habitSlice';
+import { habits } from '../App'
 
-const CreateHabit = () => {
+const CreateHabit = (props) => {
+
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+
+  const dispatch = useDispatch()
+
+  const AddHabitHandler = () => {
+    const currentHabit = { title, description }
+    habits.push(currentHabit)
+    console.log(habits)
+    if(habits.length !== 0){
+      localStorage.setItem('newHabit', JSON.stringify(habits))
+    }
+    dispatch(addHabit({ title, description }))
+
+    props.onHide()
+  }
+
   return (
-    <div>CreateHabit</div>
+    <>
+      <Model {...props} size='sm' aria-labelledby='contained-model-title-vcenter' centered>
+        <Model.Dialog className='model-sm'>
+          <Model.Body>
+            <Form.Group controlId='title'>
+              <Form.Label>Habit Title</Form.Label>
+              <Form.Control 
+              type='text' 
+              placeholder='Enter Title' 
+              value={title} 
+              onChange={(e) => setTitle(e.target.value)}></Form.Control>
+
+            </Form.Group>
+
+            <Form.Group controlId='description'>
+              <Form.Label>Describe it </Form.Label>
+              <Form.Control
+                type='text'
+                placeholder='short description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}></Form.Control>
+            </Form.Group>
+
+            <Model.Footer>
+              <Button onClick={() => AddHabitHandler()}>Add Habit</Button>
+            </Model.Footer>
+
+
+
+          </Model.Body>
+        </Model.Dialog>
+      </Model>
+    </>
   )
 }
 
