32a88b7b5d073197103ed779d1016caa362e9f01
kin6sman <domin12313xyz@gmail.com>
2023-05-31 10:09:26 +0530

    creating components for app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
 import React from 'react'
-import Home from './components/Home'
+
 import './App.css'
+import Header from './components/Header'
+import Overview from './components/Overview'
+import Details from './components/Details'
+import { Row, Col, Container } from 'react-bootstrap'
+
+var habits = []
+export { habits}
+
 
 function App() {
   return (
-    <div className='main-container'>
-      <Home/>
-    </div>
+    <>
+    <Header />
+    <Container>
+      <Row className='mt-5'>
+        <Col md={3}>
+          <Overview />
+        </Col>
+        <Col md={9}>
+          <Details />
+        </Col>
+      </Row>
+    </Container>
+  </>
   )
 }
 
