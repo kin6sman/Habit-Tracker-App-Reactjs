90958645c6aff9bbb61cd68050cf248d7cbfab69
kin6sman <domin12313xyz@gmail.com>
2023-06-01 19:38:52 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import Home from './components/Home'
-// import './App.css'
+import './App.css'
 import Header from './components/Header'
 
 // function App() {
@@ -16,7 +16,7 @@ import Header from './components/Header'
 
 import React from 'react';
 import { ThemeProvider } from '@mui/material/styles';
-// import { Container } from '@mui/material';
+import { Container } from '@mui/material';
 
 import theme from './theme';
 
@@ -24,12 +24,13 @@ const App = () => {
   return (
     <ThemeProvider theme={theme}>
     <Header/>
-
+{/* <Container maxWidth="xl" sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}> */}
     
     <Home />
-
-      
+    {/* </Container> */}
+    <h6>Created by Rahul Sharma</h6>  
     </ThemeProvider>
+
   );
 };
 
